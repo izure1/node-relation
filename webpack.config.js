0756605778a062e40b57dc19c4313e74744b3393
izure <=>
2020-09-08 00:56:27 +0900

    fixed module bug

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        NodeRelation: path.join(__dirname, 'src', 'NodeRelation.ts')
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js',
+        libraryTarget: 'umd',
+        library: 'NodeRelation'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+}
