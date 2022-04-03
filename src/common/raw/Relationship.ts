5f6deec15e643b53a533918d9b53502ec42a8a63
izure <izure@naver.com>
2022-04-03 10:06:19 +0900

    update zeroVector jsdoc description

--- src/common/raw/Relationship.ts
+++ src/common/raw/Relationship.ts
@@ -102,7 +102,7 @@ export class Relationship<T> {
   }
 
   /**
-   * Get a 1-dimensional vector that was filled 0. The vector's size are same as nodes length of instance. This is useful for representing data that does not belong to anything.
+   * Get a 1-dimensional vector that was filled 0. The vector's size are same as nodes length of instance. This is useful for representing data that does not belong to anything. It could be used as dataset for machine learning.
    * @example
    * const voidVector = state.zeroHot
    * const vectors = Array.from(state.oneHot.values())
